af4fe960ab07c4c70e0ffdc70825c11c754218ee
Rehno Lindeque <errantkid@gmail.com>
2012-03-27 10:56:34 -0700

    Update the script to fetch the latest js libs from the net

--- fetch-js-libs.sh
+++ fetch-js-libs.sh
@@ -1,2 +1,3 @@
 wget -O static/socket.io/socket.io.js https://raw.github.com/LearnBoost/socket.io-client/master/dist/socket.io.js
-# TODO: Fetch jquery
+wget -O static/adt/adt.js https://raw.github.com/rehno-lindeque/adt.js/master/dist/adt.js
+wget -O static/jquery/jquery.min.js http://code.jquery.com/jquery-latest.min.js
