d2a56d5cf5c25db98263e3306de6089a80e56dcf
Rehno Lindeque <errantkid@gmail.com>
2012-05-06 16:43:51 -0700

    Parse messages on the client received from the server

--- src/TestInspectorPage.hs
+++ src/TestInspectorPage.hs
@@ -44,25 +44,41 @@ pageHtml = renderHtml [shamlet|
 -- Embedded javascript in the html page
 --pageJs = renderJavascript $ [js|
 pageJs = [st|
-    var Message = adt('Acknowledge','Notify','ReloadFiles','PatchFile'),
-    createWebSocket = function(path) {
-      var host = window.location.hostname;
-      if(host == '') host = 'localhost';
-      var uri = 'ws://' + host + ':8080' + path;
+    var
+      Message = adt('Acknowledge','Notify','ReloadFiles','PatchFile', 'ParseError'),
+      evalMessage = adt({
+        Acknowledge: function() { console.log("...previous message acknowledged"); },
+        Notify: function(notification) { console.log("...notification:", notification); },
+        ReloadFiles: function(files) { console.log("...reload files: ", files); },
+        PatchFile: function(filePath, patch) {
+          console.log("...apply patch to file: ", file);
+          console.log("...patch: ", patch);
+        },
+        ParseError: function(message) { console.error("...previous sent message could not be parsed: ", message); },
+        _: function() { console.error("...(error) unknown message type `" + this._key + "`" ); }
+      }),
 
-      var Socket = 'MozWebSocket' in window ? MozWebSocket : WebSocket;
-      return new Socket(uri);
-    };
+      createWebSocket = function(path) {
+        var host = window.location.hostname;
+        if(host == '') host = 'localhost';
+        var uri = 'ws://' + host + ':8080' + path;
+
+        var Socket = 'MozWebSocket' in window ? MozWebSocket : WebSocket;
+        return new Socket(uri);
+      };
 
     $(document).ready(function () {
       var ws = createWebSocket('/');
 
       ws.onopen = function() {
+
         ws.send("Message from client");
       };
 
       ws.onmessage = function(event) {
-        console.log(event);
+        console.log("Message received...");
+        console.log("...event: ", event);
+        evalMessage(adt.deserialize(event.data));
       };
     });
   |]
