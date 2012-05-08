40151cb1d903716c53ebc7ed47fa73565f4545a9
Rehno Lindeque <errantkid@gmail.com>
2012-05-08 00:34:17 -0700

    Load javascript from files (unfortunately automatic reload doesn't work right now)

--- src/TestInspectorPage.hs
+++ src/TestInspectorPage.hs
@@ -1,13 +1,15 @@
 {-# LANGUAGE QuasiQuotes #-}
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP #-} -- to control production versus debug
 
 module TestInspectorPage (pageHtml) where
 
 import Text.Hamlet (shamlet)
 import Text.Blaze.Renderer.Utf8 (renderHtml)
---import Text.Julius (renderJavascript, js)
-import Text.Shakespeare.Text (st)
-import Text.Blaze (preEscapedText)
+import Text.Julius (JavascriptUrl, renderJavascriptUrl, jsFile, jsFileReload)
+import Text.Blaze (preEscapedLazyText)
+import qualified Data.Text.Lazy as LT (concat)
 
 -- Html page
 pageHtml = renderHtml [shamlet|
@@ -20,12 +22,6 @@ pageHtml = renderHtml [shamlet|
         <script src=/jquery/jquery.min.js>
         <script src=/adt/adt.js>
         <script>
-          /*var socket = io.connect('http://localhost');
-          socket.on('news', function (data) {
-            console.log(data);
-            socket.emit('my other event', { my: 'data' });
-          });*/
-        <script>
           ^{js}
       <body>
         <h1 .page-title>#{pageTitle}
@@ -35,56 +31,20 @@ pageHtml = renderHtml [shamlet|
         <footer>^{copyright}
   |]
   where
-    js = preEscapedText pageJs
+    js = preEscapedLazyText $ LT.concat $ map (renderJavascriptUrl (\_ _ -> undefined)) jsFiles
     pageTitle = "Chomp (A brave new LangLang compiler)" :: String
     pageSubTitle = "Test Inspector" :: String
     test1Src = "" :: String
     copyright = "Copyright (C) Rehno Lindeque."
 
--- Embedded javascript in the html page
---pageJs = renderJavascript $ [js|
-pageJs = [st|
-    var
-      Message = adt('Acknowledge','Notify','ReloadFiles','PatchFile', 'ParseError'),
-      evalMessage = adt({
-        Acknowledge: function() { console.log("...previous message acknowledged"); },
-        Notify: function(notification) { console.log("...notification:", notification); },
-        ReloadFiles: function(files) { 
-          console.log("...reload files: ", files); 
-        },
-        PatchFile: function(filePath, patch) {
-          console.log("...apply patch to file: ", file);
-          console.log("...patch: ", patch);
-        },
-        ParseError: function(message) { console.error("...previous message this client sent could not be parsed: \n", message); },
-        _: function() { console.error("...(error) unknown message type `" + this._key + "`" ); }
-      }),
-
-      sendMessage = function(ws, message) {
-        ws.send(adt.serialize(message));
-      },
-
-      createWebSocket = function(path) {
-        var host = window.location.hostname;
-        if(host == '') host = 'localhost';
-        var uri = 'ws://' + host + ':8080' + path;
-        var Socket = 'MozWebSocket' in window ? MozWebSocket : WebSocket;
-        return new Socket(uri);
-      };
-
-    $(document).ready(function () {
-      var ws = createWebSocket('/');
-
-      ws.onopen = function() {
-        //sendMessage(ws, "Message from client");
-        // TODO: do something meaningful here
-      };
-
-      ws.onmessage = function(event) {
-        console.log("Message received...");
-        console.log("...event: ", event);
-        evalMessage(adt.deserialize(event.data));
-      };
-    });
-  |]
-
+#if PRODUCTION
+jsFiles = [
+    $(jsFile "message.js"),
+    $(jsFile "websocket-service.js")
+  ]
+#else
+jsFiles = [
+    $(jsFileReload "message.js"),
+    $(jsFileReload "websocket-service.js")
+  ]
+#endif
