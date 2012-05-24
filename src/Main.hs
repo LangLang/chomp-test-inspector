330478f9264d5d3b42d61864f28a6560e1da36f8
Rehno Lindeque <errantkid@gmail.com>
2012-05-24 01:28:05 -0700

    Added handlers for common io exceptions when the directory to watch isn't available

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,17 @@
+{-# LANGUAGE  CPP #-}
 module Main (main) where
 
 -- Standard modules
+import Data.Text (pack)
+import Data.Text.IO (hPutStrLn)
 import qualified Network.Wai.Handler.Warp as Warp
 import Network.Wai.Handler.WebSockets (interceptWith)
 import Network.WebSockets(defaultWebSocketsOptions)
+import System.IO (stderr)
+import System.IO.Error (try, ioeGetErrorType, IOErrorType)
+#ifdef __GLASGOW_HASKELL__
+import qualified GHC.IO.Exception as Exception
+#endif
 
 -- Application modules
 import WebApp
@@ -19,13 +27,33 @@ main = do
   fileStore <- STM.FileStore.newIO
   --serverMessages <- STM.Messages.newIO -- TODO
   -- Run asynchronous observers
-  observerId <- forkFileObserver watchPath fileStore
-  -- Run the front controllers
-  Warp.runSettings (webAppSettings fileStore) webApp
-  -- Stop the asynchronous observers
-  killFileObserver observerId
+  eitherExceptionOrId <- try $ forkFileObserver watchPath fileStore
+  case eitherExceptionOrId of
+    Left e ->
+      case generateErrorMessage $ ioeGetErrorType e of 
+        Just message -> hPutStrLn stderr $ pack message
+        Nothing -> ioError e
+    Right observerId -> do 
+      -- Run the front controllers
+      Warp.runSettings (webAppSettings fileStore) webApp
+      -- Stop the asynchronous observers
+      killFileObserver observerId  
   where
     watchPath = "tests" :: FilePath
+     
+    -- Generate an error message 
+    generateErrorMessage :: IOErrorType -> Maybe String
+#ifdef __GLASGOW_HASKELL__
+    generateErrorMessage errorType = case errorType of
+      -- GHC only:
+      Exception.NoSuchThing -> Just $ "The path supplied `" ++ watchPath ++ "` does not exist."
+      Exception.PermissionDenied -> Just $ "Permission to read the the path `" ++ watchPath ++ "` was denied."      
+      Exception.InvalidArgument -> Just $ "The path supplied `" ++ watchPath ++ "` is not a valid directory name."
+      Exception.InappropriateType -> Just $ "The path supplied `" ++ watchPath ++ "` refers to a non-directory object."
+      _ -> Nothing
+#else
+    generateErrorMessage errorType = Nothing
+#endif
 
 -- Set up the web application (front controller) with the websocket application (front controller)
 -- and shared resources (the file store and incoming message queues)
@@ -34,3 +62,12 @@ webAppSettings fileStore = Warp.defaultSettings
   { Warp.settingsPort = 8080
   , Warp.settingsIntercept = interceptWith defaultWebSocketsOptions $ websocketApp fileStore
   }
+  
+  
+  
+  
+  
+  
+  
+  
+  
