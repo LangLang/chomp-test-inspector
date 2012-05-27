c2d6f1d59bff0979e9802a6ccd0b05020eaf13b0
Rehno Lindeque <errantkid@gmail.com>
2012-05-26 20:25:30 -0700

    Add the supersimple editor to the lists of libraries in the main fetch script

--- fetch-js-libs.sh
+++ fetch-js-libs.sh
@@ -1,3 +1,4 @@
 wget -O static/socket.io/socket.io.js https://raw.github.com/LearnBoost/socket.io-client/master/dist/socket.io.js
 wget -O static/adt/adt.js https://raw.github.com/rehno-lindeque/adt.js/master/dist/adt.js
 wget -O static/jquery/jquery.min.js http://code.jquery.com/jquery-latest.min.js
+wget -O static/supersimple/supersimple-editor.js https://raw.github.com/rehno-lindeque/supersimple-editor.js/master/dist/supersimple-editor.js
