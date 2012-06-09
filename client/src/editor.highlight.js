9cbaf7b7c98d99b35352048fe7e19854270f7769
Rehno Lindeque <errantkid@gmail.com>
2012-06-09 16:32:32 -0700

    Making the caret positioning a little bit more robust

--- client/src/editor.highlight.js
+++ client/src/editor.highlight.js
@@ -1,13 +1,5 @@
   var Editor = Editor || {};
   (function(html){
-    /*var processElement = adt({
-      Text: function(el) {
-        var ast = LangLang.highlight(el.data);
-        console.log("LangLang AST: ", ast);
-      },
-      _: function(attributes) { console.log("Unknown DOM element ", this._tag, this._datatype, attributes); }
-    });*/
-
     var 
       getNodeOffset = function(childNode) {
         var prevNode = childNode.previousSibling;
@@ -21,6 +13,32 @@
           return offset;
         return offset + getParentNodeOffset(parentNode, childNode.parentNode);
       },
+      getFocusPointAtOffset = function(rootNode, offset) {
+        var
+          childNode = rootNode.firstChild,
+          parentOffset = 0,
+          lastChildNode,
+          textLength,
+          descendant;
+        if (offset == 0)
+          return [rootNode, 0];
+        if (childNode === null)
+          return [rootNode, Math.min(offset, rootNode.textContent.length)];
+        while (childNode) {
+          // Skip nodes that fall behind the offset (calculate offset to parent node)
+          textLength = childNode.textContent.length;
+          if (parentOffset + textLength < offset) {
+            parentOffset += textLength;
+            childNode = childNode.nextSibling;
+            continue;
+          }
+          // The node containing the offset is some descendant of this childNode
+          descendant = getFocusPointAtOffset(childNode, offset - parentOffset);
+          return descendant;
+        }
+        // Offset is past the end of the text content
+        return getFocusPointAtOffset(rootNode.lastChild, offset);
+      },
       getCaretOffset = function(domElement) {
         if (!window.getSelection)
           return 0;
@@ -29,33 +47,28 @@
           return selection.focusOffset;
         return getParentNodeOffset(domElement, selection.focusNode) + selection.focusOffset;
       },
-      setCaretOffset = function() {
-        if (window.getSelection) {
-          var s = window.getSelection();
-          if (s.rangeCount > 0) 
-            s.removeAllRanges();
-          s.addRange(savedRange);
-        }
-        else 
-          if (document.createRange)
-            window.getSelection().addRange(savedRange);
+      setCaretOffset = function(domElement, offset) {
+        var
+          selection = window.getSelection(),
+          focusPoint = getFocusPointAtOffset(domElement, offset),
+          range = document.createRange();
+        // Remove existing selections
+        if (window.getSelection)
+          if (selection.rangeCount > 0) 
+            selection.removeAllRanges();
+        range.setStart(focusPoint[0], focusPoint[1]);
+        range.setEnd(focusPoint[0], focusPoint[1]);
+        selection.addRange(range);
       };
-
 
     Editor.highlight = function(domElement) {
       var 
         result = LangLang.highlight(domElement.textContent),
-        i;
-      console.log(result);
-      console.log("Caret offset: ", getCaretOffset(domElement));
+        i,
+        caretOffset = getCaretOffset(domElement);
       domElement.innerHTML = "";
       for (var i = 0; i < result.length; ++i)
         domElement.appendChild(result[i]);
-      /*
-      var child = domElement.firstChild;
-      while (child) {
-        processElement(child);
-        child = child.nextSibling;
-      }*/
+      setCaretOffset(domElement, caretOffset);
     };
   })(html.evalCons);
