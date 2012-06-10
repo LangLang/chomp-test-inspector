de196011be2e01eaa4d153e1a188374b9987fb58
Rehno Lindeque <errantkid@gmail.com>
2012-06-10 16:23:01 -0700

    Fix newline-related problems with the editor highlighting (Take divs into account as newlines when calculating the caret offset)

--- client/src/editor.highlight.js
+++ client/src/editor.highlight.js
@@ -1,16 +1,27 @@
   var Editor = Editor || {};
   (function(html){
-    var 
+    var
+      getTextContent = function(domElement) {
+        return domElement.innerText? domElement.innerText : domElement.textContent;
+      },
       getNodeOffset = function(childNode) {
-        var prevNode = childNode.previousSibling;
+        var 
+          prevNode = childNode.previousSibling,
+          offset = 0;
+        // If this is a div then it moves children and next siblings to the next line
+        // (Chromium inserts divs for newlines with contenteditable elements)
+        if (childNode["tagName"] && childNode["tagName"] === 'DIV')
+          ++offset;
         if (!prevNode)
-          return 0;
-        return prevNode.textContent.length + getNodeOffset(prevNode);
+          return offset;
+        return offset + getTextContent(prevNode).length + getNodeOffset(prevNode);
       },
       getParentNodeOffset = function(parentNode, childNode) {
         var offset = getNodeOffset(childNode);
         if (childNode.parentNode === parentNode)
           return offset;
+        // If this is a div then it moves children and next siblings to the next line
+        // (Chromium inserts divs for newlines with contenteditable elements)
         return offset + getParentNodeOffset(parentNode, childNode.parentNode);
       },
       getFocusPointAtOffset = function(rootNode, offset) {
@@ -23,11 +34,11 @@
         if (offset == 0)
           return [rootNode, 0];
         if (childNode === null)
-          return [rootNode, Math.min(offset, rootNode.textContent.length)];
+          return [rootNode, Math.min(offset, getTextContent(rootNode).length)];
         while (childNode) {
           // Skip nodes that fall behind the offset (calculate offset to parent node)
-          textLength = childNode.textContent.length;
-          if (parentOffset + textLength < offset) {
+          textLength = getTextContent(childNode).length;
+          if (parentOffset + textLength <= offset) {
             parentOffset += textLength;
             childNode = childNode.nextSibling;
             continue;
@@ -39,13 +50,36 @@
         // Offset is past the end of the text content
         return getFocusPointAtOffset(rootNode.lastChild, offset);
       },
+      getFocusInSelection = function(selection, node) {
+          var 
+            text = getTextContent(selection.focusNode),
+            focusOffset = selection.focusOffset,
+            i;
+          /* Count the number of newlines before the caret offset inside this node
+          console.log("FOCUS NODE", selection.focusNode, selection.focusOffset);
+          for (i = 0; i < focusOffset; ++i) {
+            if (text[i] === '\n') {
+              console.log("NEWLINE");
+              ++focusOffset;
+            }
+          }
+          if (focusOffset == 0) {
+            // Google-chrome inserts divs in order to create new-lines... so in
+            // the case that we're inside a div, add one to the focus offset
+            console.log(selection.focusNode.parentNode.parentNode.parentNode);
+            //for (var j = 0; j < selection.focusNode.parentNode.parentNode.parentNode.parentNode.children.length; ++j)
+            //  console.log(selection.focusNode.parentNode.parentNode.parentNode.parentNode.children[j]);
+          }
+          console.log("        --offset: ", focusOffset);*/
+          return focusOffset;
+      },
       getCaretOffset = function(domElement) {
         if (!window.getSelection)
           return 0;
         var selection = window.getSelection();
         if (selection.focusNode === domElement)
-          return selection.focusOffset;
-        return getParentNodeOffset(domElement, selection.focusNode) + selection.focusOffset;
+          return getFocusInSelection(selection);
+        return getParentNodeOffset(domElement, selection.focusNode) + getFocusInSelection(selection);
       },
       setCaretOffset = function(domElement, offset) {
         var
@@ -64,7 +98,7 @@
     Editor.highlight = function(domElement) {
       var 
         originalCaretPos = getCaretOffset(domElement),
-        result = LangLang.highlight(domElement.textContent, originalCaretPos),
+        result = LangLang.highlight(getTextContent(domElement), originalCaretPos),
         i;
       domElement.innerHTML = "";
       for (var i = 0; i < result.html.length; ++i)
