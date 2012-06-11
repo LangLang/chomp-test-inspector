ea18d69eeac5fa40a1d628aade6a89498b29d4d7
Rehno Lindeque <errantkid@gmail.com>
2012-06-10 19:28:27 -0700

    Ignore special keys when refreshing the editor

--- client/src/editor.events.js
+++ client/src/editor.events.js
@@ -2,6 +2,13 @@
   (function(){
     Editor.bindEvents = function() {
       $('#editors').on('keyup', '.supersimple-editor-input', function(e){
+        var code = e.charCode? e.charCode : e.keyCode;
+        // Ignore arrow keys
+        if (code >= 37 && code <= 40)
+          return;
+        // Ignore shift/control/alt
+        if (code == 0 || (code >= 16 && code <= 18))
+          return;
         Editor.highlight(e.target);
       });
     };
